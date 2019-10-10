429d9edc6b09366d4cb26b24738634308dc1c29f
Sci-Ga <gabor.varga87@hotmail.com>
2019-10-10 10:24:25 +0200

    ok

--- src/views/EnyemView.vue
+++ src/views/EnyemView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="enyem">
+ <div class="enyem">
     <h1>Ez itt az enyém</h1>
   </div>
 </template>
@@ -8,4 +8,6 @@
 import { Component, Vue } from "vue-property-decorator";
 @Component
 export default class EnyemView extends Vue {}
+
 </script>
+
